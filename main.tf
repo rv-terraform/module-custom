fdd3ca98a72834dfd07069ec55a93bb5ea474f6e
venerari <35533668+venerari@users.noreply.github.com>
2018-02-16 14:11:52 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,17 +3,17 @@ module mycompute {
     resource_group_name = "${var.resource_group_name}"
     location = "${var.location}"
     admin_username = "${var.adminuser}"
-    ssh_key = "~/.ssh/id_rsa.pub"
-    vm_os_simple = "RedHat"
-    vm_os_offer  = "RHEL"
-    vm_os_sku    = "7-RAW"
     remote_port = "22"
-    nb_instances = 1
     public_ip_address_allocation = "dynamic"
     public_ip_dns = ["venerari03"]
     vnet_subnet_id = "${module.network.vnet_subnets[0]}"
+    vm_os_simple = "RedHat"
+    vm_os_offer  = "RHEL"
+    vm_os_sku    = "7-RAW"
     storage_account_type = "Standard_LRS"
+    ssh_key = "~/.ssh/id_rsa.pub"
     vm_size = "Standard_DS1_V2"
+    nb_instances = 1
     tags {
         environment = "${var.env}"
     }
@@ -32,9 +32,9 @@ output "vm_public_name" {
     value = "${module.mycompute.public_ip_dns_name}"
 }
 
-output "vm_public_ip" {
-    value = "${module.mycompute.public_ip_address}"
-}
+#output "vm_public_ip" {
+#    value = "${module.mycompute.public_ip_address}"
+#}
 
 output "vm_private_ips" {
     value = "${module.mycompute.network_interface_private_ip}"
