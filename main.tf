71ff120a2f63645b1faecaa223454cd5c73f4f9b
venerari <35533668+venerari@users.noreply.github.com>
2018-02-16 13:38:09 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,19 +4,24 @@ module mycompute {
     location = "${var.location}"
     admin_username = "${var.adminuser}"
     ssh_key = "~/.ssh/id_rsa.pub"
-    vm_os_simple = "CentOS"
+    vm_os_simple = "RedHat"
+    vm_os_offer  = "RHEL"
+    vm_os_sku    = "7-RAW"
     remote_port = "22"
-    nb_instances = 2
+    nb_instances = 1
     public_ip_address_allocation = "dynamic"
     public_ip_dns = ["venerari123"]
     vnet_subnet_id = "${module.network.vnet_subnets[0]}"
     storage_account_type = "Standard_LRS"
+    vm_size = "Standard_DS1_V2"
+    tags = "${var.environment}"
 }
 
 module "network" {
     source = "Azure/network/azurerm"
     location = "${var.location}"
     resource_group_name = "${var.resource_group_name}"
+    tags = "${var.environment}"
 }
 
 output "vm_public_name" {
